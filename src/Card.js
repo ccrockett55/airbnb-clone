c3dda82a994a5d2e25a3f3f35fb82e23906f82e0
Li Crockett <ccrockett55@gmail.com>
2022-02-16 23:16:45 -0600

    Added review section

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,46 @@
 import React from "react";
 import life from './life.png'
+import review1 from './review1.png'
+import wedding from './wedding.png'
+import review2 from './review2.png'
+import bike from './bike.png'
+import review3 from './review3.png'
 
 function Card() {
     return (
         <div>
-            <img src={life} className='picsize' />
+            <div className='float'>
+            <table>
+                <tr>
+                    <td className='tablestyle'>
+                    <img src={life} className='picsize' /><br/>
+                    </td>  
+                    <td className='tablestyle'>
+                    <img src={wedding} className='picsize' /><br/>
+                    </td>  
+                    <td className='tablestyle'>
+                    <img src={bike} className='picsize' /><br/>
+                    </td>  
+                </tr>
+                <tr>
+                    <td className='tablestyle'>
+                    <img src={review1} /> 
+                    </td>  
+                    <td className='tablestyle'>
+                    <img src={review2} /> 
+                    </td>  
+                    <td className='tablestyle'>
+                    <img src={review3} /> 
+                    </td>  
+                </tr>
+            </table>
+           
+            
+            </div>
+          
         </div>
     );
 }
 
 export default Card;
+
